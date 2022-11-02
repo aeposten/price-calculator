8b17f3427efa0736c89248b6cd9a2e32346e4015
Amy <86963506+aeposten@users.noreply.github.com>
2022-11-02 11:10:54 -0700

    Currency converter and reset functions work

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const KEY = "uQzNyhlY04NNv7TsWKPYVyrEqTQAKZIW";
+
 let hourlyRateEl = selectComponent("hourly-rate");
 let hoursEl = selectComponent("hours");
 let materialsEl = selectComponent("materials");
 let shippingEl = selectComponent("shipping");
 let markupEl = selectComponent("markup");
-let usdEl = selectComponent("usd");
+let USDEl = selectComponent("USD");
+let finalPrice = "";
 
 function selectComponent(elementId) {
   let component = document.getElementById(elementId);
@@ -15,6 +18,42 @@ function generatePrice() {
     parseInt(hourlyRateEl.value) * parseInt(hoursEl.value) +
     parseInt(materialsEl.value) +
     parseInt(shippingEl.value);
-  let finalPrice = baseTotal * parseInt(markupEl.value);
-  usdEl.textContent = finalPrice;
+  finalPrice = baseTotal * parseInt(markupEl.value);
+  USDEl.textContent = finalPrice;
+  return finalPrice;
 }
+
+function fetchCurrency(to, from) {
+  generatePrice();
+  try {
+    fetch(
+      `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${finalPrice}`,
+      {
+        headers: {
+          apikey: KEY,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let converted = data.result;
+        let element = selectComponent(to);
+        if (document.readyState === "loading") {
+          element.textContent = "Loading from API";
+        } else {
+          element.textContent = converted;
+        }
+      });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function resetPage() {
+    selectComponent('USD').textContent = 'Price Appears Here'
+    selectComponent('CAD').textContent = 'Price Appears Here'
+    selectComponent('EUR').textContent = 'Price Appears Here'
+}
+
+// fetchCurrency('CAD', 'USD', finalPrice)
+// fetchCurrency('EUR', 'USD', finalPrice)
